418020e3f0efa42d440834791db3824e751e9aeb
Garrett Gruss <garrett.gruss@gmail.com>
2025-02-23 22:32:21 -0600

    Roller added, code added

--- controllers/joystickControl/joystickControl.java
+++ controllers/joystickControl/joystickControl.java
@@ -7,7 +7,8 @@ public class joystickControl {
 
     private static final int TIME_STEP = 32;
     private static final double MAX_SPEED = 10;
-    private static final double GRABBER_ATTITUDE_SPEED = 100;
+    private static final double GRABBER_ATTITUDE_SPEED = 30;
+    private static final double GRABBER_ROLLER_SPEED = 10;
 
     public static void main(String[] args) {
         Robot robot = new Robot();
@@ -22,16 +23,22 @@ public class joystickControl {
         "front_left_motor", "front_right_motor", 
         "rear_left_motor", "rear_right_motor", 
         "middle_left_motor", "middle_right_motor",
-        "grabber_attitude"};
+        "grabber_attitude", "grabber_roller_motor"};
         
         Motor[] motors = new Motor[motorNames.length];
 
         for (int i = 0; i < motorNames.length; i++) {
             motors[i] = robot.getMotor(motorNames[i]);
             motors[i].setPosition(Double.POSITIVE_INFINITY); // Set motor to velocity control mode
-            //motors[i].setVelocity(0.0); // Initialize motor velocity to 0
+            motors[i].setVelocity(0.0); // Initialize motor velocity to 0
             //motors[i].setAvailableTorque(0.0); // Force motors to not apply braking force when idle
         }
+        
+        boolean grabber_roller_toggle = false;
+        
+        System.out.println("Use arrow keys to drive");
+        System.out.println("Use Space to lift ball grabber");
+        System.out.println("Press End to toggle roller");
 
         while (robot.step(TIME_STEP) != -1) {
             // Read joystick axes
@@ -43,6 +50,7 @@ public class joystickControl {
             double left = 0;
             double right = 0;
             double grabber_attitude = 0;
+            double grabber_roller = 0;
             
             int key;
             while ((key = keyboard.getKey()) != -1) {  // Process all pressed keys
@@ -51,6 +59,14 @@ public class joystickControl {
                 if (key == Keyboard.LEFT) left = 1;
                 if (key == Keyboard.RIGHT) right = 1;
                 if (key == 32) grabber_attitude = -1; // if key is space
+                if (key == Keyboard.END) {
+                  grabber_roller_toggle = !grabber_roller_toggle;
+                  if (grabber_roller_toggle) {
+                    System.out.println("Roller On");
+                  } else {
+                    System.out.println("Roller Off");
+                  } // shift key
+                }
             }
             
             double leftStickY = forward - backward;
@@ -61,6 +77,11 @@ public class joystickControl {
             double leftSpeed = (leftStickY + rightStickX) * MAX_SPEED;
             double rightSpeed = (leftStickY - rightStickX) * MAX_SPEED;
             double grabber_attitude_speed = grabber_attitude * GRABBER_ATTITUDE_SPEED;
+            double grabber_roller_speed = 0;
+            if (grabber_roller_toggle) {
+              grabber_roller_speed = -GRABBER_ROLLER_SPEED;
+            }
+            // System.out.println("grabber roller is: " + grabber_roller_toggle);
             
             // Limit wheel speeds to maximum
             leftSpeed = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, leftSpeed));
@@ -75,6 +96,7 @@ public class joystickControl {
             motors[4].setTorque(leftSpeed); // middle_left_motor
             motors[5].setTorque(rightSpeed); // middle_right_motor
             motors[6].setTorque(grabber_attitude_speed); // grabber_attitude
+            motors[7].setTorque(grabber_roller_speed); // grabber_roller_motor
             //System.out.println("grabber speed = " + grabber_attitude_speed);
         }
     }
